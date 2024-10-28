8d08788382bd738c52e37742c9aecd0f3f4cb30c
Malv741 <malvcronos@gmail.com>
2024-10-28 15:53:51 -0300

    segundo commit

--- Versionamento/src/App.java
+++ Versionamento/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        mostraMensagem();
         System.out.println("Hello, World!");
+    }
+    
+    public static void mostraMensagem(){
+        System.out.println("Bem vindo!! Abaixo temos um Hello World!\n");
     }
 }
